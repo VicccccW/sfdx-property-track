c87e10a8feca7bb248519f48b1c5ed5746913708
Victor Wang <victor.wang@open.edu.au>
2020-03-12 01:38:12 +1100

    add button

--- force-app/main/default/lwc/systemConfigSetting/systemConfigSetting.js
+++ force-app/main/default/lwc/systemConfigSetting/systemConfigSetting.js
@@ -6,9 +6,9 @@ export default class systemConfigSetting extends LightningElement {
   //DO NOT mark to @api or @track as we want this property immutable
   //if we want to change the custom setting value, we can create a copy and update that one
   systemConfigFields;
-  
+
   @track hasCustomFields;
-  
+
   @track error;
 
   @wire(getSystemConfigFields)
@@ -24,20 +24,53 @@ export default class systemConfigSetting extends LightningElement {
   }
 
   checkHasCustomFields() {
-      this.hasCustomFields = this.systemConfigFields.some(el => el.isCustom === true);
+    this.hasCustomFields = this.systemConfigFields.some(el => el.isCustom === true);
   }
 
   get customFields() {
-    const customFields = this.systemConfigFields.filter(el => el.isCustom == true);
+    const customFields = this.systemConfigFields.filter(el => el.isCustom === true);
     console.log('customFields are ', customFields);
     return customFields;
   }
-  
-  //after get the data, which is an array
-  //use a filter to find out the isCustom = true item
+
+
   //also need a variable to store the id
-  
+
 
   //we also need another method to call an apex method to update the custom setting in Salesforce
+
+  //function to control button 
+  //if click edit button, which is enabled by default
+  //enabled the input tags
+  //disabled edit button 
+
+  //if any input field changed
+  //enabled save button
+
+  //if click save button 
+  //toast to say wheter save success or fail
+  //call apex method to save
+  //if success ->
+  //disable input tags
+  //enable edit button
+  //if fail ->
+  //toast the error message
+  //restore value 
+  //enable eidt button
+
+  handleEdit(event) {
+    this.template.querySelectorAll('button').forEach(element => {
+      if (element.name === "Edit") {
+        element.setAttribute("disabled", null);
+      } else if(element.name === "Save") {
+        element.removeAttribute("disabled", null);
+      }
+    });
+    
+  }
+
+  handleSave(event) {
+
+  }
 
 }

ac131477d121441f47a84d257eeed79ae52675f7
Victor Wang <victor.wang@open.edu.au>
2020-03-12 16:31:08 +1100

    custom setting update lwc

--- force-app/main/default/lwc/systemConfigSetting/systemConfigSetting.js
+++ force-app/main/default/lwc/systemConfigSetting/systemConfigSetting.js
@@ -1,5 +1,8 @@
 import { LightningElement, wire, track } from 'lwc';
 import getSystemConfigFields from '@salesforce/apex/SystemConfigController.getSystemConfigFields';
+import setSystemConfigFields from '@salesforce/apex/SystemConfigController.setSystemConfigFields';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { reduceErrors } from 'c/idsUtils';
 
 export default class systemConfigSetting extends LightningElement {
 
@@ -7,7 +10,13 @@ export default class systemConfigSetting extends LightningElement {
   //if we want to change the custom setting value, we can create a copy and update that one
   systemConfigFields;
 
+  customSettingId;
+
+  newSysConfigStr;
+
   @track hasCustomFields;
+
+  @track customFields;
 
   @track error;
 
@@ -20,57 +29,96 @@ export default class systemConfigSetting extends LightningElement {
     } else if (result.error) {
       this.systemConfigFields = undefined;
       this.error = result.error;
+      this.hasCustomFields = false;
     }
+
+    if (this.hasCustomFields) {
+      this.customFields = this.systemConfigFields.filter(el => el.isCustom === true);
+    }
+
   }
 
   checkHasCustomFields() {
+    this.customSettingId = this.systemConfigFields.find(el => el.name === 'Id').value;
+    console.log('custom setting sf id is ' + this.customSettingId);
     this.hasCustomFields = this.systemConfigFields.some(el => el.isCustom === true);
   }
 
-  get customFields() {
-    const customFields = this.systemConfigFields.filter(el => el.isCustom === true);
-    console.log('customFields are ', customFields);
-    return customFields;
-  }
-
-
-  //also need a variable to store the id
-
-
-  //we also need another method to call an apex method to update the custom setting in Salesforce
-
-  //function to control button 
-  //if click edit button, which is enabled by default
-  //enabled the input tags
-  //disabled edit button 
-
-  //if any input field changed
-  //enabled save button
-
-  //if click save button 
-  //toast to say wheter save success or fail
-  //call apex method to save
-  //if success ->
-  //disable input tags
-  //enable edit button
-  //if fail ->
-  //toast the error message
-  //restore value 
-  //enable eidt button
-
   handleEdit(event) {
     this.template.querySelectorAll('button').forEach(element => {
-      if (element.name === "Edit") {
+      if (element.name === "EditBtn") {
         element.setAttribute("disabled", null);
-      } else if(element.name === "Save") {
+      } else if (element.name === "SaveBtn") {
         element.removeAttribute("disabled", null);
       }
+
+      this.template.querySelectorAll("[name='fieldValueInput']").forEach(element => {
+        element.removeAttribute("disabled", null)
+      });
+
     });
-    
   }
 
   handleSave(event) {
+    this.template.querySelectorAll('button').forEach(element => {
+      if (element.name === "EditBtn") {
+        element.removeAttribute("disabled", null);
+      } else if (element.name === "SaveBtn") {
+        element.setAttribute("disabled", null);
+      }
 
+      this.template.querySelectorAll("[name='fieldValueInput']").forEach(element => {
+        element.setAttribute("disabled", null)
+      });
+
+    });
+
+    this.buildNewSysConfigStr();
+
+    setSystemConfigFields({ newSysConfigStr: this.newSysConfigStr })
+      .then(result => {
+        if (result.status === 'Success') {
+          this.dispatchEvent(
+            new ShowToastEvent({
+              title: 'Success',
+              message: 'Update SystemConifg setting Successlly!',
+              variant: 'success'
+            })
+          );
+        } else if (result.status === 'Error') {
+          this.dispatchEvent(
+            new ShowToastEvent({
+              title: 'Error updating SystemConifg setting.',
+              message: result.messages.join(', '),
+              variant: 'error'
+            })
+          );
+        }
+      })
+      .catch(error => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: 'Error updating SystemConifg setting.',
+            message: reduceErrors(error).join(', '),
+            variant: 'error'
+          })
+        );
+      });
   }
 
+  buildNewSysConfigStr() {
+    const newStrObj = {
+      Id: this.customSettingId
+    };
+
+    this.template.querySelectorAll("input[name='fieldValueInput']").forEach(el => {
+      newStrObj[el.title] = el.value;
+    });
+
+    console.log('return obj to Apex');
+    console.log(newStrObj);
+
+    //format should be '{"Id": "a02N000000JJJ8dIAH", "Property_Backup_Scan_Range_Days__c": "10"}'
+    this.newSysConfigStr = JSON.stringify(newStrObj);
+  }
 }

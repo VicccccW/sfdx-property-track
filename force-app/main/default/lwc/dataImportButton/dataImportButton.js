25cfc46101ae6dd17c72832968d15b69c282b13c
Victor Wang <victor.wang@open.edu.au>
2020-03-12 17:32:09 +1100

    edit property controller

--- force-app/main/default/lwc/dataImportButton/dataImportButton.js
+++ force-app/main/default/lwc/dataImportButton/dataImportButton.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class DataImportButton extends LightningElement {}
+export default class DataImportButton extends LightningElement {
+
+  handleClick(event) {
+    //call apex method, which in turn make the callout the external api and return a update status, success or error
+  }
+
+}

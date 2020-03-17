7244fbbf382f920ce0e0f9e56e6ba7f34d8f8a72
Victor Wang <victor.wang@open.edu.au>
2020-03-17 19:18:27 +1100

    implement settimeout for open new tab

--- force-app/main/default/lwc/propertyMap/propertyMap.js
+++ force-app/main/default/lwc/propertyMap/propertyMap.js
@@ -1,15 +1,16 @@
 import { LightningElement, track, wire } from 'lwc';
 import getMapPropertyList from '@salesforce/apex/PropertyController.getMapPropertyList';
+import { NavigationMixin } from 'lightning/navigation';
 
-
-export default class PropertyMap extends LightningElement {
+export default class PropertyMap extends NavigationMixin(LightningElement) {
 
   center = {
     location: {
       City: 'Epping',
       Country: 'AUS',
       PostalCode: '3076'
-    }
+    },
+    title: 'Epping'
   };
 
   //center markers title
@@ -24,6 +25,8 @@ export default class PropertyMap extends LightningElement {
   @track mapMarkers = [];
 
   @track error;
+
+  @track selectedMarkerValue;
 
   constructor() {
     super();
@@ -42,7 +45,8 @@ export default class PropertyMap extends LightningElement {
       this.mapMarkers = undefined;
       this.error = 'No property available.';
     } else if (result.data) {
-      this.mapMarkers = this.buildMapMarkers(result.data);
+      //this.mapMarkers = this.buildMapMarkers(result.data);
+      this.buildMapMarkers(result.data);
       this.error = undefined;
     } else if (result.error) {
       this.mapMarkers = undefined;
@@ -51,28 +55,59 @@ export default class PropertyMap extends LightningElement {
   }
 
   buildMapMarkers(propertyList) {
-    const locationList = [];
-
     propertyList.forEach(element => {
-      const marker = {
-        location: {}
-      };
-      marker.location.Street = element.No_of_Rooms__c + ' Rooms, ' 
-                              + element.No_of_Bathrooms__c + ' Bathrooms.' 
-      marker.location.Latitude = element.Geolocation__Latitude__s;
-      marker.location.Longitude = element.Geolocation__Longitude__s;
-      marker.icon = 'standard:home';
-      marker.title = element.Name__c;
-      marker.description = element.No_of_Rooms__c + ' Rooms, ' 
-                          + element.No_of_Bathrooms__c + ' Bathrooms.' 
-                          + '</br>' 
-                          + '$' + element.Est_Min_Price__c
-                          + ' ~ '
-                          + '$' + element.Est_Max_Price__c;
-                          
-      locationList.push(marker);
+      this.mapMarkers = [...this.mapMarkers,
+      {
+        location: {
+          Street: `${element.No_of_Rooms__c} Rooms, ${element.No_of_Bathrooms__c} Bathrooms.`,
+          Latitude: element.Geolocation__Latitude__s,
+          Longitude: element.Geolocation__Longitude__s,
+        },
+        value: element.Id,
+        icon: 'standard:home',
+        title: element.Name__c,
+        description: `${element.No_of_Rooms__c} Rooms, ${element.No_of_Bathrooms__c} Bathrooms.`
+          + '</br>'
+          + `$${element.Est_Min_Price__c} ~ $${element.Est_Max_Price__c}`
+      }]
     });
 
-    return locationList;
-  }  
+    // const locationList = [];
+
+    // propertyList.forEach(element => {
+    //   const marker = {
+    //     location: {}
+    //   };
+    //   marker.location.Street = element.No_of_Rooms__c + ' Rooms, ' 
+    //                           + element.No_of_Bathrooms__c + ' Bathrooms.' 
+    //   marker.location.Latitude = element.Geolocation__Latitude__s;
+    //   marker.location.Longitude = element.Geolocation__Longitude__s;
+    //   marker.icon = 'standard:home';
+    //   marker.title = element.Name__c;
+    //   marker.description = element.No_of_Rooms__c + ' Rooms, ' 
+    //                       + element.No_of_Bathrooms__c + ' Bathrooms.' 
+    //                       + '</br>' 
+    //                       + '$' + element.Est_Min_Price__c
+    //                       + ' ~ '
+    //                       + '$' + element.Est_Max_Price__c;
+
+    //   locationList.push(marker);
+    // });
+
+    // return locationList;
+  }
+
+  handleMarkerSelect(event) {
+    setTimeout(this.openNewSubTab, 1500, this, event.target.selectedMarkerValue);
+  }
+
+  openNewSubTab(that, id) {
+      that[NavigationMixin.Navigate]({
+          type: 'standard__recordPage',
+          attributes: {
+              recordId: id,
+              actionName: 'view'
+          }
+      });
+  }
 }
